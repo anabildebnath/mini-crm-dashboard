eba3eb7e270a8ed6ed984914b3c3f156aaa0f112
Anabil Debnath <73880095+anabildebnath@users.noreply.github.com>
2025-04-26 15:27:05 +0600

    vite setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,24 +1,35 @@
-import React from 'react';
-import { Route, Switch } from 'react-router-dom';
-import Dashboard from './pages/Dashboard';
-import Customers from './pages/Customers';
-import Upload from './pages/Upload';
-import Login from './pages/Login';
-import Registration from './pages/Registration';
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
 function App() {
+  const [count, setCount] = useState(0)
+
   return (
-    <div className="App">
-      <Switch>
-        <Route path="/login" component={Login} />
-        <Route path="/register" component={Registration} />
-        <Route path="/dashboard" component={Dashboard} />
-        <Route path="/customers" component={Customers} />
-        <Route path="/upload" component={Upload} />
-        <Route path="/" component={Dashboard} />
-      </Switch>
-    </div>
-  );
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
 }
 
-export default App;
+export default App
